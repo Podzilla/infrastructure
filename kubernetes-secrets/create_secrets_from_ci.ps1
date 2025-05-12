2b447215a24da471a5da36058d62aeaaa86066a0
Nour Eldien Ayman <nourayman189@gmail.com>
2025-05-12 12:19:18 +0300

    Add PersistentVolumeClaims and Services for ERP, Order, Warehouse, and Courier databases

--- kubernetes-secrets/create_secrets_from_ci.ps1
+++ kubernetes-secrets/create_secrets_from_ci.ps1
@@ -6,6 +6,7 @@
 kubectl create secret generic auth-secrets `
   --from-literal=SPRING_DATASOURCE_USERNAME="$env:AUTH_DB_USERNAME" `
   --from-literal=SPRING_DATASOURCE_PASSWORD="$env:AUTH_DB_PASSWORD" `
+  --from-literal=AUTH_DB_NAME="$env:AUTH_DB_NAME" `
   --from-literal=SECRET_KEY="$env:AUTH_SECRET_KEY" `
   --namespace=dev `
   --dry-run=client -o yaml |
@@ -15,6 +16,7 @@ kubeseal --format yaml --cert kubeseal-cert.pem > kubernetes-secrets/auth-sealed
 kubectl create secret generic erp-secrets `
   --from-literal=SPRING_DATASOURCE_USERNAME="$env:ERP_DB_USERNAME" `
   --from-literal=SPRING_DATASOURCE_PASSWORD="$env:ERP_DB_PASSWORD" `
+  --from-literal=ERP_DB_NAME="$env:ERP_DB_NAME" `
   --namespace=dev `
   --dry-run=client -o yaml |
 kubeseal --format yaml --cert kubeseal-cert.pem > kubernetes-secrets/erp-sealed-secret.yaml
@@ -23,6 +25,7 @@ kubeseal --format yaml --cert kubeseal-cert.pem > kubernetes-secrets/erp-sealed-
 kubectl create secret generic order-secrets `
   --from-literal=SPRING_DATASOURCE_USERNAME="$env:ORDER_DB_USERNAME" `
   --from-literal=SPRING_DATASOURCE_PASSWORD="$env:ORDER_DB_PASSWORD" `
+  --from-literal=ORDER_DB_NAME="$env:ORDER_DB_NAME" `
   --namespace=dev `
   --dry-run=client -o yaml |
 kubeseal --format yaml --cert kubeseal-cert.pem > kubernetes-secrets/order-sealed-secret.yaml
@@ -31,6 +34,7 @@ kubeseal --format yaml --cert kubeseal-cert.pem > kubernetes-secrets/order-seale
 kubectl create secret generic warehouse-secrets `
   --from-literal=SPRING_DATASOURCE_USERNAME="$env:WAREHOUSE_DB_USERNAME" `
   --from-literal=SPRING_DATASOURCE_PASSWORD="$env:WAREHOUSE_DB_PASSWORD" `
+  --from-literal=WAREHOUSE_DB_NAME="$env:WAREHOUSE_DB_NAME" `
   --namespace=dev `
   --dry-run=client -o yaml |
 kubeseal --format yaml --cert kubeseal-cert.pem > kubernetes-secrets/warehouse-sealed-secret.yaml
@@ -39,6 +43,7 @@ kubeseal --format yaml --cert kubeseal-cert.pem > kubernetes-secrets/warehouse-s
 kubectl create secret generic cart-secrets `
   --from-literal=SPRING_DATA_MONGODB_USERNAME="$env:CART_MONGO_USERNAME" `
   --from-literal=SPRING_DATA_MONGODB_PASSWORD="$env:CART_MONGO_PASSWORD" `
+  --from-literal=CART_DB_NAME="$env:CART_DB_NAME" `
   --namespace=dev `
   --dry-run=client -o yaml |
 kubeseal --format yaml --cert kubeseal-cert.pem > kubernetes-secrets/cart-sealed-secret.yaml
@@ -47,46 +52,59 @@ kubeseal --format yaml --cert kubeseal-cert.pem > kubernetes-secrets/cart-sealed
 kubectl create secret generic courier-secrets `
   --from-literal=SPRING_DATA_MONGODB_USERNAME="$env:COURIER_MONGO_USERNAME" `
   --from-literal=SPRING_DATA_MONGODB_PASSWORD="$env:COURIER_MONGO_PASSWORD" `
+  --from-literal=COURIER_DB_NAME="$env:COURIER_DB_NAME" `
   --namespace=dev `
   --dry-run=client -o yaml |
 kubeseal --format yaml --cert kubeseal-cert.pem > kubernetes-secrets/courier-sealed-secret.yaml
 
-kubectl create secret generic mongo-secrets `
-  --from-literal=CART_MONGO_USERNAME="$env:CART_MONGO_USERNAME" `
-  --from-literal=CART_MONGO_PASSWORD="$env:CART_MONGO_PASSWORD" `
-  --from-literal=CART_MONGO_DB="$env:CART_MONGO_DB" `
-  --from-literal=COURIER_MONGO_USERNAME="$env:COURIER_MONGO_USERNAME" `
-  --from-literal=COURIER_MONGO_PASSWORD="$env:COURIER_MONGO_PASSWORD" `
-  --from-literal=COURIER_MONGO_DB="$env:COURIER_MONGO_DB" `
-  --from-literal=MONGO_INITDB_ROOT_USERNAME="$env:MONGO_INITDB_ROOT_USERNAME" `
-  --from-literal=MONGO_INITDB_ROOT_PASSWORD="$env:MONGO_INITDB_ROOT_PASSWORD" `
+kubectl create secret generic mongo-cart-secrets `
+  --from-literal=MONGO_INITDB_ROOT_USERNAME="$env:CART_MONGO_USERNAME" `
+  --from-literal=MONGO_INITDB_ROOT_PASSWORD="$env:CART_MONGO_PASSWORD" `
   --namespace=dev `
   --dry-run=client -o yaml |
-kubeseal --format yaml --cert kubeseal-cert.pem > kubernetes-secrets/mongo-sealed-secret.yaml
+kubeseal --format yaml --cert kubeseal-cert.pem > kubernetes-secrets/mongo-cart-sealed-secret.yaml
+
+kubectl create secret generic mongo-courier-secrets `
+  --from-literal=MONGO_INITDB_ROOT_USERNAME="$env:COURIER_MONGO_USERNAME" `
+  --from-literal=MONGO_INITDB_ROOT_PASSWORD="$env:COURIER_MONGO_PASSWORD" `
+  --namespace=dev `
+  --dry-run=client -o yaml |
+kubeseal --format yaml --cert kubeseal-cert.pem > kubernetes-secrets/mongo-courier-sealed-secret.yaml
 
 # PostgreSQL secrets (all services)
-kubectl create secret generic psql-secrets `
-  --from-literal=POSTGRES_USER="$env:POSTGRES_USER" `
-  --from-literal=POSTGRES_PASSWORD="$env:POSTGRES_PASSWORD" `
-  --from-literal=POSTGRES_DB="$env:POSTGRES_DB" `
-  --from-literal=AUTH_DB_USERNAME="$env:AUTH_DB_USERNAME" `
-  --from-literal=AUTH_DB_PASSWORD="$env:AUTH_DB_PASSWORD" `
-  --from-literal=AUTH_DB_NAME="$env:AUTH_DB_NAME" `
-  --from-literal=ERP_DB_USERNAME="$env:ERP_DB_USERNAME" `
-  --from-literal=ERP_DB_PASSWORD="$env:ERP_DB_PASSWORD" `
-  --from-literal=ERP_DB_NAME="$env:ERP_DB_NAME" `
-  --from-literal=ORDER_DB_USERNAME="$env:ORDER_DB_USERNAME" `
-  --from-literal=ORDER_DB_PASSWORD="$env:ORDER_DB_PASSWORD" `
-  --from-literal=ORDER_DB_NAME="$env:ORDER_DB_NAME" `
-  --from-literal=WAREHOUSE_DB_USERNAME="$env:WAREHOUSE_DB_USERNAME" `
-  --from-literal=WAREHOUSE_DB_PASSWORD="$env:WAREHOUSE_DB_PASSWORD" `
-  --from-literal=WAREHOUSE_DB_NAME="$env:WAREHOUSE_DB_NAME" `
+kubectl create secret generic psql-auth-secrets `
+  --from-literal=POSTGRES_USER="$env:AUTH_DB_USERNAME" `
+  --from-literal=POSTGRES_PASSWORD="$env:AUTH_DB_PASSWORD" `
+  --from-literal=POSTGRES_DB="$env:AUTH_DB_NAME" `
   --namespace=dev `
   --dry-run=client -o yaml |
-kubeseal --format yaml --cert kubeseal-cert.pem > kubernetes-secrets/psql-sealed-secret.yaml
+kubeseal --format yaml --cert kubeseal-cert.pem > kubernetes-secrets/psql-auth-sealed-secret.yaml
 
-kubectl apply -f kubernetes-secrets/mongo-sealed-secret.yaml
-kubectl apply -f kubernetes-secrets/psql-sealed-secret.yaml
+kubectl create secret generic psql-erp-secrets `
+  --from-literal=POSTGRES_USER="$env:ERP_DB_USERNAME" `
+  --from-literal=POSTGRES_PASSWORD="$env:ERP_DB_PASSWORD" `
+  --from-literal=POSTGRES_DB="$env:ERP_DB_NAME" `
+  --namespace=dev `
+  --dry-run=client -o yaml |
+kubeseal --format yaml --cert kubeseal-cert.pem > kubernetes-secrets/psql-erp-sealed-secret.yaml
+
+kubectl create secret generic psql-order-secrets `
+  --from-literal=POSTGRES_USER="$env:ORDER_DB_USERNAME" `
+  --from-literal=POSTGRES_PASSWORD="$env:ORDER_DB_PASSWORD" `
+  --from-literal=POSTGRES_DB="$env:ORDER_DB_NAME" `
+  --namespace=dev `
+  --dry-run=client -o yaml |
+kubeseal --format yaml --cert kubeseal-cert.pem > kubernetes-secrets/psql-order-sealed-secret.yaml
+
+kubectl create secret generic psql-warehouse-secrets `
+    --from-literal=POSTGRES_USER="$env:WAREHOUSE_DB_USERNAME" `
+    --from-literal=POSTGRES_PASSWORD="$env:WAREHOUSE_DB_PASSWORD" `
+    --from-literal=POSTGRES_DB="$env:WAREHOUSE_DB_NAME" `
+    --namespace=dev `
+    --dry-run=client -o yaml |
+kubeseal --format yaml --cert kubeseal-cert.pem > kubernetes-secrets/psql-warehouse-sealed-secret.yaml
+
+
 
 kubectl apply -f kubernetes-secrets/auth-sealed-secret.yaml
 kubectl apply -f kubernetes-secrets/erp-sealed-secret.yaml
@@ -94,3 +112,9 @@ kubectl apply -f kubernetes-secrets/order-sealed-secret.yaml
 kubectl apply -f kubernetes-secrets/warehouse-sealed-secret.yaml
 kubectl apply -f kubernetes-secrets/cart-sealed-secret.yaml
 kubectl apply -f kubernetes-secrets/courier-sealed-secret.yaml
+kubectl apply -f kubernetes-secrets/mongo-cart-sealed-secret.yaml
+kubectl apply -f kubernetes-secrets/mongo-courier-sealed-secret.yaml
+kubectl apply -f kubernetes-secrets/psql-auth-sealed-secret.yaml
+kubectl apply -f kubernetes-secrets/psql-erp-sealed-secret.yaml
+kubectl apply -f kubernetes-secrets/psql-order-sealed-secret.yaml
+kubectl apply -f kubernetes-secrets/psql-warehouse-sealed-secret.yaml
